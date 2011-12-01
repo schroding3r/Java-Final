738e27ea521346f8caaac8f911ba4cc4c9fc7580
schroding3r <jarrett.kyle@gmail.com>
2011-12-01 14:54:34 -0500

    Added interface selection for easier testing

--- Main.java
+++ Main.java
@@ -1,10 +1,25 @@
 package banking;
-
+import java.util.Scanner;
 public class Main 
 {
 	public static void main(String Args[])
 	{
-		Interact.menu();
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("1)GUI\n2)CLI");
+		int c=scanner.nextInt();
+		switch(c)
+		{
+			case 1:{
+					System.out.println("Starting GUI...");
+					GUI gui = new GUI();
+					gui.main();
+					break;
+					}
+			case 2:{
+					System.out.println("Starting CLI...");
+					Interact.menu();
+					break;
+					}
+		}
 	}
-
 }
